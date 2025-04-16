b6c0ca7f3816a91ba53875c1999d03e6e04f049f
Geir Sagberg <geir.sagberg@gmail.com>
2025-04-16 19:20:05 +0200

    Nice

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -2,6 +2,7 @@ import Map from 'ol/Map'
 import Overlay from 'ol/Overlay'
 import View from 'ol/View'
 import { pointerMove } from 'ol/events/condition'
+import { createEmpty, extend } from 'ol/extent'
 import { GeoJSON } from 'ol/format'
 import Select from 'ol/interaction/Select'
 import TileLayer from 'ol/layer/Tile'
@@ -24,12 +25,24 @@ proj4.defs(
 register(proj4)
 
 // Kristiansand coordinates in WGS84 (EPSG:4326)
-const KRISTIANSAND_COORDS = [8.0187, 58.1464]
+const TRONDHEIM_COORDS = [10.3932, 63.4305]
 
-const FEATURE_THRESHOLD = 10000
+const DEBOUNCE_TIME = 100
 
 export interface MapRef {
   drawVeglenkerInView: () => Promise<void>
+}
+
+// Debounce function
+function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout | null = null
+  return (...args: Parameters<T>) => {
+    if (timeout) clearTimeout(timeout)
+    timeout = setTimeout(() => func(...args), wait)
+  }
 }
 
 export const VegkartMap = forwardRef<MapRef>((_, ref) => {
@@ -127,7 +140,7 @@ export const VegkartMap = forwardRef<MapRef>((_, ref) => {
     }
 
     const kristiansandUTM = transform(
-      KRISTIANSAND_COORDS,
+      TRONDHEIM_COORDS,
       'EPSG:4326',
       'EPSG:25833',
     )
@@ -157,6 +170,90 @@ export const VegkartMap = forwardRef<MapRef>((_, ref) => {
       overlays: [popupOverlay.current],
     })
 
+    // Redraw features after zooming with debounce
+    const debouncedRedraw = debounce(() => {
+      if (mapRef.current) {
+        const mapRefObj = ref as React.RefObject<MapRef>
+        mapRefObj.current?.drawVeglenkerInView()
+      }
+    }, DEBOUNCE_TIME)
+
+    map.getView().on('change:resolution', debouncedRedraw)
+    map.getView().on('change:center', debouncedRedraw)
+
+    // Add click interaction for clusters
+    map.on('click', (e) => {
+      const feature = map.forEachFeatureAtPixel(e.pixel, (f) => f)
+      if (feature) {
+        const size = feature.get('features')?.length
+        if (size > 1) {
+          // Get all features in the cluster
+          const features = feature.get('features')
+          // Create an extent that contains all features
+          const extent = features.reduce((extent: number[], f: any) => {
+            return extend(extent, f.getGeometry().getExtent())
+          }, createEmpty())
+
+          // Add some padding to the extent
+          const padding = 100 // meters
+          extent[0] -= padding
+          extent[1] -= padding
+          extent[2] += padding
+          extent[3] += padding
+
+          // Zoom to the extent
+          map.getView().fit(extent, {
+            duration: 500,
+            padding: [50, 50, 50, 50],
+          })
+          popupOverlay.current!.setPosition(undefined)
+          return
+        }
+        const properties = feature.getProperties()
+        const popupContent = `
+          <div class="p-2 bg-white rounded shadow">
+            <div class="font-bold mb-2">Veglenke ${properties.veglenkesekvensId}-${properties.veglenkenummer}</div>
+            <table class="text-sm">
+              <tr>
+                <td class="pr-2 font-medium">Lengde:</td>
+                <td>${properties.lengde.toFixed(1)}m</td>
+              </tr>
+              <tr>
+                <td class="pr-2 font-medium">Kommune:</td>
+                <td>${properties.kommune}</td>
+              </tr>
+              <tr>
+                <td class="pr-2 font-medium">Startposisjon:</td>
+                <td>${properties.startposisjon}</td>
+              </tr>
+              <tr>
+                <td class="pr-2 font-medium">Sluttposisjon:</td>
+                <td>${properties.sluttposisjon}</td>
+              </tr>
+              <tr>
+                <td class="pr-2 font-medium">Startdato:</td>
+                <td>${properties.startdato}</td>
+              </tr>
+              ${
+                properties.sluttdato
+                  ? `
+                <tr>
+                  <td class="pr-2 font-medium">Sluttdato:</td>
+                  <td>${properties.sluttdato}</td>
+                </tr>
+              `
+                  : ''
+              }
+            </table>
+          </div>
+        `
+        popupRef.current!.innerHTML = popupContent
+        popupOverlay.current!.setPosition(e.coordinate)
+      } else {
+        popupOverlay.current!.setPosition(undefined)
+      }
+    })
+
     // Add hover interaction
     const select = new Select({
       condition: pointerMove,
@@ -166,11 +263,6 @@ export const VegkartMap = forwardRef<MapRef>((_, ref) => {
     select.on('select', (e) => {
       const feature = e.selected[0]
       if (feature) {
-        const size = feature.get('features')?.length
-        if (size > 1) {
-          popupOverlay.current!.setPosition(undefined)
-          return
-        }
         const properties = feature.getProperties()
         const popupContent = `
           <div class="p-2 bg-white rounded shadow">
